ae03e648813a9746c24d5af18fd19a6f87444383
HanWool Lee <kudnya@gmail.com>
2020-12-09 00:40:52 +0900

    add DistrictCodeDAO

--- app/src/main/java/tech/hanwool/caraccount/database/dao/DistrictCodeDao.kt
+++ app/src/main/java/tech/hanwool/caraccount/database/dao/DistrictCodeDao.kt
@@ -4,20 +4,23 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Transaction
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
 import tech.hanwool.caraccount.database.model.DistrictCode
 
 @Dao
 interface DistrictCodeDao {
     @Query("SELECT * FROM district_codes")
-    fun getAll(): List<DistrictCode>
+    fun getAll(): Observable<List<DistrictCode>>
 
     @Query("SELECT * FROM district_codes WHERE name = :name")
-    fun get(name: String): DistrictCode
+    fun get(name: String): Single<DistrictCode>
 
     @Insert
-    fun insert(code: DistrictCode)
+    fun insert(code: DistrictCode): Completable
 
     @Transaction
     @Insert
-    fun insertAll(codes: List<DistrictCode>)
+    fun insertAll(codes: List<DistrictCode>): Completable
 }
